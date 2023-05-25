ec0e2a1dd1726111bba966be035e8a34b3d8f839
dlgudtmd <dlgudtmd1022@gmail.com>
2023-05-25 21:12:57 +0900

    Baek 2755 재업로드

--- src/Bronze/Baek2755.java
+++ src/Bronze/Baek2755.java
@@ -23,38 +23,38 @@ public class Baek2755 {
             int score = (int)(arr[i][1].charAt(0) - 48);
             count += score;
             String grade = arr[i][2];
-            if(arr[i][2].equals("A+")){
+            if(grade.equals("A+")){
                 total += (double)score * 4.3;
-            }else if(arr[i][2].equals("A0")){
+            }else if(grade.equals("A0")){
                 total += (double)score * 4.0;
-            }else if(arr[i][2].equals("A-")){
+            }else if(grade.equals("A-")){
                 total += (double)score * 3.7;
             }
-            else if(arr[i][2].equals("B+")){
+            else if(grade.equals("B+")){
                 total += (double)score * 3.3;
             }
-            else if(arr[i][2].equals("B0")){
+            else if(grade.equals("B0")){
                 total += (double)score * 3.0;
             }
-            else if(arr[i][2].equals("B-")){
+            else if(grade.equals("B-")){
                 total += (double)score * 2.7;
             }
-            else if(arr[i][2].equals("C+")){
+            else if(grade.equals("C+")){
                 total += (double)score * 2.3;
             }
-            else if(arr[i][2].equals("C0")){
+            else if(grade.equals("C0")){
                 total += (double)score * 2.0;
             }
-            else if(arr[i][2].equals("C-")){
+            else if(grade.equals("C-")){
                 total += (double)score * 1.7;
             }
-            else if(arr[i][2].equals("D+")){
+            else if(grade.equals("D+")){
                 total += (double)score * 1.3;
             }
-            else if(arr[i][2].equals("D0")){
+            else if(grade.equals("D0")){
                 total += (double)score * 1.0;
             }
-            else if(arr[i][2].equals("D-")){
+            else if(grade.equals("D-")){
                 total += (double)score * 0.7;
             }
         }
