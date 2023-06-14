9b012ee77b66a22ae8c55fd14389163d4e512e03
이형승 <dlgudtmd1022@gmail.com>
2023-06-14 09:52:07 +0900

    Baek 9012

--- src/Silver4/Baek9012.java
+++ src/Silver4/Baek9012.java
@@ -1,9 +1,8 @@
 package Silver4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
+
 
 public class Baek9012 {
     public static void main(String[] args) throws IOException {
@@ -11,20 +10,22 @@ public class Baek9012 {
 
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
+        for(int i = 0; i < T ; i ++){
+            String word = br.readLine();
             Stack<Character> stack = new Stack<>();
-            String vps = br.readLine();
-            for (int j = 0; j < vps.length(); j++) {
-                char word = vps.charAt(j);
-                if(word == '('){
-                    stack.push(word);
-                }else {
+
+            for (int j = 0 ; j < word.length(); j++){
+                char vpsW = word.charAt(j);
+
+                if(vpsW == '('){
+                    stack.push(vpsW);
+                } else {
                     if(stack.isEmpty()){
-                    stack.push(word);
-                    break;
-                        }   else {
-                            stack.pop();
-                        }
+                        stack.push(vpsW);
+                        break;
+                    } else{
+                        stack.pop();
+                    }
                 }
             }
             if(stack.isEmpty()){
