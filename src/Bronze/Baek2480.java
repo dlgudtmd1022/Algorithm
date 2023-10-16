1f3f591c0caffcc35d1680351e5183c5d070112a
unknown <dlgudtmd1022@gmail.com>
2023-10-16 17:56:42 +0900

    Baek2480

---
+++ src/Bronze/Baek2480.java
@@ -0,0 +1,36 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek2480 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int [] arr = new int[3];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        int max = arr[0];
+        if(arr[0] == arr[1] && arr[0] == arr[2]){
+            result = 10000 + arr[0] * 1000;
+        } else if (arr[0] == arr[1] || arr[0] == arr[2] || arr[1] == arr[2]) {
+            if(arr [0] == arr[1]){
+                result = 1000 + arr[0] * 100;
+            } else if (arr[1] == arr[2]) {
+                result = 1000 + arr[1] * 100;
+            } else {
+                result = 1000 + arr[2] * 100;
+            }
+        }else {
+            result = Math.max(Math.max(arr[0], arr[1]), arr[2]) * 100;
+        }
+        System.out.println(result);
+    }
+}
