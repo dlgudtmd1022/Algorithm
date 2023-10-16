038ca188d66f539b5ab4dd2a4e2f73b7c5c764ae
unknown <dlgudtmd1022@gmail.com>
2023-10-16 16:33:35 +0900

    Baek2750r

--- src/Bronze/Baek2750.java
+++ src/Bronze/Baek2750.java
@@ -1,8 +1,6 @@
 package Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 public class Baek2750 {
@@ -11,14 +9,15 @@ public class Baek2750 {
 
         int N = Integer.parseInt(br.readLine());
 
-        int arr [] = new int[N];
+        int[] arr = new int[N];
 
-        for (int i = 0; i < arr.length; i++){
+        for(int i = 0; i < N ; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++){
+        for(int i = 0; i < N; i++){
             System.out.println(arr[i]);
         }
+        br.close();
     }
 }
